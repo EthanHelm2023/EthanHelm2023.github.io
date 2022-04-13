20a032d516b788fb6cee879d1a99a9572b112a28
Helm Ethan <824851@stma.k12.mn.us>
2022-04-13 12:28:38 -0500

    Push

--- quiz.js
+++ quiz.js
@@ -1,93 +1,34 @@
 var q1 = document.getElementById("q1");
-var q1result = document.getElementById("q1result");
-var s1 = document.getElementById("s1");
+var q2 = document.getElementById("q1");
+var q3 = document.getElementById("q1");
+var q4 = document.getElementById("q1");
+var q5 = document.getElementById("q1");
+var submit = document.getElementById("submit");
+var score = document.getElementById("score");
+var result = 0;
 
-
-function checkq1() {
+function calculate() {
 	if (q1.value == "Option 3"){
 		q1result.innerHTML = "Correct";
 	}
 	
-	else{
-		q1result.innerHTML = "Wrong";
-		
-	}
-	
-}
-
-s1.addEventListener("click", checkq1);
-
-var q2 = document.getElementById("q1");
-var q2result = document.getElementById("q1result");
-var s2 = document.getElementById("s1");
-
-function checkq2() {
 	if (q2.value == "Option 3"){
-		q2result.innerHTML = "Correct";
+		result = result + 1;
 	}
 	
-	else{
-		q2result.innerHTML = "Wrong";
-		
-	}
-	
-}
-
-s2.addEventListener("click", checkq2);
-
-var q3 = document.getElementById("q1");
-var q3result = document.getElementById("q1result");
-var s3 = document.getElementById("s1");
-
-function checkq3() {
 	if (q3.value == "Option 3"){
-		q3result.innerHTML = "Correct";
+		result = result + 1;
 	}
 	
-	else{
-		q3result.innerHTML = "Wrong";
-		
-	}
-	
-}
-
-s3.addEventListener("click", checkq3);
-
-var q4 = document.getElementById("q1");
-var q4result = document.getElementById("q1result");
-var s4 = document.getElementById("s1");
-
-function checkq4() {
 	if (q4.value == "Option 3"){
-		q4result.innerHTML = "Correct";
+		result = result + 1;
 	}
 	
-	else{
-		q4result.innerHTML = "Wrong";
-		
-	}
-	
-}
-
-s4.addEventListener("click", checkq4);
-
-var q5 = document.getElementById("q1");
-var q5result = document.getElementById("q1result");
-var s5 = document.getElementById("s1");
-
-function checkq5() {
 	if (q5.value == "Option 3"){
-		q5result.innerHTML = "Correct";
+		result = result + 1;
 	}
-	
-	else{
-		q5result.innerHTML = "Wrong";
-		
-	}
-	
 }
 
-s5.addEventListener("click", checkq5);
-
+result.addEventListener("click", submit);
 
 
