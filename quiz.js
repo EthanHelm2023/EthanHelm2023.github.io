d814eecd873762dfc7f6c1e355975b52ee59471d
Helm Ethan <824851@stma.k12.mn.us>
2022-04-14 12:22:24 -0500

    stuff

--- quiz.js
+++ quiz.js
@@ -1,34 +1,72 @@
-var q1 = document.getElementById("q1");
-var q2 = document.getElementById("q1");
-var q3 = document.getElementById("q1");
-var q4 = document.getElementById("q1");
-var q5 = document.getElementById("q1");
-var submit = document.getElementById("submit");
-var score = document.getElementById("score");
-var result = 0;
+var b1 = document.getElementById("lemon");
+var b2 = document.getElementById("orange");
+var b3 = document.getElementById("banana");
+var b4 = document.getElementById("grape");
+var b5 = document.getElementById("apple");
+var b6 = document.getElementById("lemon1");
+var b7 = document.getElementById("orange1");
+var b8 = document.getElementById("banana1");
+var b9 = document.getElementById("grape1");
+var b10 = document.getElementById("apple1");
+var image = document.getElementById("image");
 
-function calculate() {
-	if (q1.value == "Option 3"){
-		q1result.innerHTML = "Correct";
-	}
-	
-	if (q2.value == "Option 3"){
-		result = result + 1;
-	}
-	
-	if (q3.value == "Option 3"){
-		result = result + 1;
-	}
-	
-	if (q4.value == "Option 3"){
-		result = result + 1;
-	}
-	
-	if (q5.value == "Option 3"){
-		result = result + 1;
-	}
+function changeLEMON() {
+	image.src = "Images/Lemon.jpg";
 }
 
-result.addEventListener("click", submit);
+function changeORANGE() {
+	image.src = "Images/Orange-Fruit-Pieces.jpg";
+
+}
+
+function changeBANANA() {
+	image.src = "Images/Banana-Single.jpg";
+
+}
+
+function changeGRAPE() {
+	image.src = "Images/file18622.jpg";
+
+}
+
+function changeAPPLE() {
+	image.src = "Images/istockphoto-1195562565-612x612.jpg";
+
+}
+
+function changeLEMON1() {
+	image.src = "Images/Lemon.jpg";
+}
+
+function changeORANGE1() {
+	image.src = "Images/Orange-Fruit-Pieces.jpg";
+
+}
+
+function changeBANANA1() {
+	image.src = "Images/Banana-Single.jpg";
+
+}
+
+function changeGRAPE1() {
+	image.src = "Images/file18622.jpg";
+
+}
+
+function changeAPPLE1() {
+	image.src = "Images/istockphoto-1195562565-612x612.jpg";
+
+}
+
+b1.addEventListener("click", changeLEMON);
+b2.addEventListener("click", changeORANGE);
+b3.addEventListener("click", changeBANANA);
+b4.addEventListener("click", changeGRAPE);
+b5.addEventListener("click", changeAPPLE);
+b6.addEventListener("click", changeLEMON1);
+b7.addEventListener("click", changeORANGE1);
+b8.addEventListener("click", changeBANANA1);
+b9.addEventListener("click", changeGRAPE1);
+b10.addEventListener("click", changeAPPLE1);
 
 
